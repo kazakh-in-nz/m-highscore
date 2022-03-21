0b575996cda09999d29d54123ff781ce9628d5d3
Nurzhan Nursultanov <nursultanov.nurzhan@gmail.com>
2022-03-21 16:38:11 +1300

    initial commit

---
+++ internal/server/grpc/grpc.go
@@ -0,0 +1,27 @@
+package grpc
+
+import (
+	"context"
+
+	pbhighscore "github.com/kazakh-in-nz/m_apis"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+)
+
+type Grpc struct {
+	address string
+	srv     *grpc.Server
+}
+
+var HighScore = 9999999999999.0
+
+func (g *Grpc) SetHighScore(ctx context.Context, input *pbhighscore.SetHighScoreRequest) (*pbhighscore.SetHighScoreResponse, error) {
+	log.Info().Msg("SetHighScore in m-highscore is called")
+	HighScore = input.HighScore
+	return &pbhighscore.SetHighScoreResponse{Set: true}, nil
+}
+
+func (g *Grpc) GetHighScore(ctx context.Context, input *pbhighscore.GetHighScoreRequest) (*pbhighscore.GetHighScoreResponse, error) {
+	log.Info().Msg("GetHighScore in m-highscore is called")
+	return &pbhighscore.GetHighScoreResponse{HighScore}, nil
+}
